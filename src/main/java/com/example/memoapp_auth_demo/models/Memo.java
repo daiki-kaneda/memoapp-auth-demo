69176c9f6b553e5037a0a946a422d23f36d5d803
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-14 01:10:08 +0900

    Add Memo,User entities

---
+++ src/main/java/com/example/memoapp_auth_demo/models/Memo.java
@@ -0,0 +1,24 @@
+package com.example.memoapp_auth_demo.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Memo {
+    @Id
+    private String id;
+
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_uid")
+    private User user;
+}
---
+++ src/main/java/com/example/memoapp_auth_demo/models/User.java
@@ -0,0 +1,25 @@
+package com.example.memoapp_auth_demo.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private String uid;
+
+    private String email;
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Memo> memos = new ArrayList<>();
+}
