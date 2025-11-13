e895dd90e283784a2c8319f29dd96829fb557492
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-14 00:55:30 +0900

    Initial commit

---
+++ src/main/java/com/example/memoapp_auth_demo/MemoappAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.memoapp_auth_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoappAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoappAuthDemoApplication.class, args);
+	}
+
+}
